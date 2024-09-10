859a432f4eba20390fb64499f547f4be1dfb5486
semakiller <semen.balashov.02@gmail.com>
2024-09-10 15:04:08 +0300

    Some map(idk it should be)

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-enum cell{
+typedef enum cell{
     WALL,
     MOVABLE
-}
+}cell;
 //#include <iostream>
 int main() {
     int width, height;
     printf("Введите размеры карты как два целых числа(ширина, высота) c любым разделителем(не выходите пж за пределы оперативки)\n");
-    if scanf("%d %d",&width, &height) != 2 {
+    if (scanf("%d %d",&width, &height) != 2) {
         printf("n/a");
     }
     else {
@@ -23,7 +23,17 @@ int main() {
             map[i][j] = rand()%2; 
         }//0 is for wall, 1 is for mov
     }
-    FILE* fptr = fopen("maps/maps.txt", "a+");
+    for (int i = 0; i <height; i++) {
+        for (int j = 0; j < width; j++) {
+            printf("%d ",map[i][j]); 
+        }//0 is for wall, 1 is for mov
+        printf("\n");
+    }
+    for (int i = 0; i < width; i++) {
+        free(map[i]);
+    }
+    free(map);
+    //FILE* fptr = fopen("maps/maps.txt", "a+");
     }
     return 0;
 }
