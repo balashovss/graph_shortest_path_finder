d7d2bd41a56420645f85ac4bcc815ec146972fe9
semakiller <semen.balashov.02@gmail.com>
2024-09-17 16:58:59 +0300

    Допиливаем генерацию лабиринта с помощью клеточного автомата

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define ALIVE_PROBABILITY 45
 typedef enum cell{
     WALL,
     MOVABLE
@@ -12,18 +13,40 @@ cell** create_matrix( int height, int width) {
     }
     return map;
 }
-void fill_map(cell** map, int height, int width, int alive_probability) {
+int aliveness_counter(cell** map, int height, int width) {
+    int alive_counter = 0;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (map[i][j] == WALL) alive_counter++;
+        }
+    }
+    return alive_counter;
+}
+void fill_map(cell** map, int height, int width) {
     srand(time(NULL));
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            if (rand()%100 < alive_probability) map[i][j] = MOVABLE;
+            if (rand()%100 < ALIVE_PROBABILITY) map[i][j] = MOVABLE;
         }//0 is for wall, 1 is for mov
     }
 }
 void matrix_print(cell** map, int height, int width) {
     int flag = 0;
-    for (int i = 0; ((i < height) && (!flag)); i++) {
-        for (int j = 0; ((j < width) && (!flag)); j++) {
+    for (int i = -1; ((i <= height) && (!flag)); i++) {
+        for (int j = -1; ((j <= width) && (!flag)); j++) {
+            if (i < 0) {
+                if (j < 0) printf("+");
+                else if (j == width) printf("+");
+                else printf("-");
+            }
+            else if (i == height) {
+                if (j < 0) printf("+");
+                else if (j == width) printf("+");
+                else printf("-");
+            }
+            else {
+            if ((j < 0) || (j == width)) printf("|");
+            else {
             switch (map[i][j]){ 
                 case WALL: printf("0");
                 break;
@@ -32,10 +55,13 @@ void matrix_print(cell** map, int height, int width) {
                 default: 
                 flag = 1;
                 break;
-            }    
+            }
+            }
+        }    
         }//0 is for wall, 1 is for mov
         printf("\n");
     }
+    printf("\n\n\n\n\n");
 }
 void free_matrix(cell*** map, int height) {
     if (map != NULL) {
@@ -55,36 +81,47 @@ int aliveNeighbours(cell** map, int height, int width, int i, int j) {
     }
     return alive_counter;
 }
+void checkAliveness(cell** map, cell** next_iteration_map_, int i, int j, int height, int width) {
+    int aliveNeighbours_ = aliveNeighbours(map, height, width, i, j);
+    if (map[i][j] == WALL) {
+        if (aliveNeighbours_ == 2) next_iteration_map_[i][j] = WALL;
+        else next_iteration_map_[i][j] = MOVABLE;
+    }
+    else {
+        if (aliveNeighbours_ == 2) next_iteration_map_[i][j] = WALL;
+        else next_iteration_map_[i][j] = MOVABLE;
+    }
+}
 cell** next_iteration_map(cell** map, int height, int width) {
-    cell** next_iteration_map_ = NULL;
+    cell** next_iteration_map_ = create_matrix(height, width);
     fill_map(next_iteration_map_, height, width, 0);
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            int aliveNeighbours_ = aliveNeighbours(map, height, width, i, j);
-            if (map[i][j] == WALL) {
-                if ((aliveNeighbours_ == 2) || (aliveNeighbours_ == 3)) next_iteration_map_[i][j] = WALL;
-                else next_iteration_map_[i][j] = MOVABLE;
-            }
-            else {
-                if (aliveNeighbours_ == 3) next_iteration_map_[i][j] = WALL;
-                else next_iteration_map_[i][j] = MOVABLE;
-            }
+            checkAliveness(map, next_iteration_map_, i, j, height, width);
         }
     }
     return next_iteration_map_;
 }
+void cellularAutomataCycle(cell** map, int height, int width) {
+    int number_of_iterations = 2;
+    for (int i = 0; i < number_of_iterations; i++) {
+        matrix_print(map, height, width);
+        map = next_iteration_map(map, height, width);
+    }
+    map[0][0] = MOVABLE;
+    map[height - 1][width - 1] = MOVABLE;
+    matrix_print(map, height, width);
+}
 int main() {
     int width, height;
-    char c;
     printf("Введите размеры карты как два целых числа(ширина, высота) c любым разделителем(не выходите пж за пределы оперативки)\n");
-    if (scanf("%d %c %d",&width, &c, &height) != 3) {
+    if (scanf("%d%d",&width, &height) != 2) {
         printf("n/a");
     }
     else {
     cell** map = create_matrix(height, width);
-    int alive_probability = 45;
-    fill_map(map, height, width, alive_probability);
-    matrix_print(map, height, width);
+    fill_map(map, height, width);
+    cellularAutomataCycle(map, height, width);
     free_matrix(&map, height);
     //FILE* fptr = fopen("maps/maps.txt", "a+");
     }
