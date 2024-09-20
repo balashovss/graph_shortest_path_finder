a3c302b49f2c89020fe9d2a2d360b6bdd8d38475
semakiller <semen.balashov.02@gmail.com>
2024-09-20 17:22:28 +0300

    Fixing git errors

--- MazeProject1/matrix_funcs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "maze_gen_funcs.h"
-#include <stdlib.h>
-#define CELL_TYPE 0
-#define INT_TYPE 1
-
-void** create_matrix(int height, int width, int regime);
-void free_matrix(void*** map, int height);
-void maze_print(cell** map, int height, int width);
--- MazeProject1/path_planning_algs.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "path_planning_algs.h"
-#include "maze_gen_funcs.h"
-void print_waves(int** wave_map, int height, int width);
-int** wave_map_init(int height, int width);
-void wave_map_step(int** wave_map, cell** map, int height, int width, int i, int j, int wave_counter, int* flag, int* wave_spreading_counter);
-void wave_map_filling(int** wave_map, cell** map, int height, int width);
-void wave_algorithm(cell*** map, int height, int width);
