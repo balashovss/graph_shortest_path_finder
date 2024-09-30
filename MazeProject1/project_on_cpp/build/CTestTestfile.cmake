f7cca36ff07b735dc76b270edb5c38636654a40e
balashovss <90271499+balashovss@users.noreply.github.com>
2024-09-30 11:36:39 +0300

    Delete MazeProject1/project_on_cpp/build/CTestTestfile.cmake

--- MazeProject1/project_on_cpp/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/semakiller/Рабочий стол/Projects/BMSTU/MazeProject1/project_on_cpp/src
-# Build directory: /home/semakiller/Рабочий стол/Projects/BMSTU/MazeProject1/project_on_cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Maze_Cave_Project_tests "./Maze_Cave_Project_tests")
-set_tests_properties(Maze_Cave_Project_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/semakiller/Рабочий стол/Projects/BMSTU/MazeProject1/project_on_cpp/src/CMakeLists.txt;10;add_test;/home/semakiller/Рабочий стол/Projects/BMSTU/MazeProject1/project_on_cpp/src/CMakeLists.txt;0;")
