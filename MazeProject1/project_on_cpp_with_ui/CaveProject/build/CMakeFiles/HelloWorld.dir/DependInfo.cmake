9df865d5552228959850583ec22350505505c8ef
semakiller <semen.balashov.02@gmail.com>
2024-09-30 13:03:30 +0300

    Beautiful and clean directry

---
+++ MazeProject1/project_on_cpp_with_ui/CaveProject/build/CMakeFiles/HelloWorld.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/semakiller/Рабочий стол/Projects/BMSTU/MazeProject1/project_on_cpp_with_ui/CaveProject/build/HelloWorld_autogen/mocs_compilation.cpp" "CMakeFiles/HelloWorld.dir/HelloWorld_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/HelloWorld.dir/HelloWorld_autogen/mocs_compilation.cpp.o.d"
+  "/home/semakiller/Рабочий стол/Projects/BMSTU/MazeProject1/project_on_cpp_with_ui/CaveProject/src/main.cpp" "CMakeFiles/HelloWorld.dir/main.cpp.o" "gcc" "CMakeFiles/HelloWorld.dir/main.cpp.o.d"
+  "/home/semakiller/Рабочий стол/Projects/BMSTU/MazeProject1/project_on_cpp_with_ui/CaveProject/src/mainwindow.cpp" "CMakeFiles/HelloWorld.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/HelloWorld.dir/mainwindow.cpp.o.d"
+  "/home/semakiller/Рабочий стол/Projects/BMSTU/MazeProject1/project_on_cpp_with_ui/CaveProject/src/matrix_funcs.cpp" "CMakeFiles/HelloWorld.dir/matrix_funcs.cpp.o" "gcc" "CMakeFiles/HelloWorld.dir/matrix_funcs.cpp.o.d"
+  "/home/semakiller/Рабочий стол/Projects/BMSTU/MazeProject1/project_on_cpp_with_ui/CaveProject/src/maze_gen_funcs.cpp" "CMakeFiles/HelloWorld.dir/maze_gen_funcs.cpp.o" "gcc" "CMakeFiles/HelloWorld.dir/maze_gen_funcs.cpp.o.d"
+  "/home/semakiller/Рабочий стол/Projects/BMSTU/MazeProject1/project_on_cpp_with_ui/CaveProject/src/path_planning_algs.cpp" "CMakeFiles/HelloWorld.dir/path_planning_algs.cpp.o" "gcc" "CMakeFiles/HelloWorld.dir/path_planning_algs.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ MazeProject1/project_on_cpp_with_ui/CaveProject/build/CMakeFiles/HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/HelloWorld_autogen.dir/ParseCache.txt"
+  "HelloWorld_autogen"
+  "CMakeFiles/HelloWorld.dir/HelloWorld_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/HelloWorld.dir/HelloWorld_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/HelloWorld.dir/main.cpp.o"
+  "CMakeFiles/HelloWorld.dir/main.cpp.o.d"
+  "CMakeFiles/HelloWorld.dir/mainwindow.cpp.o"
+  "CMakeFiles/HelloWorld.dir/mainwindow.cpp.o.d"
+  "CMakeFiles/HelloWorld.dir/matrix_funcs.cpp.o"
+  "CMakeFiles/HelloWorld.dir/matrix_funcs.cpp.o.d"
+  "CMakeFiles/HelloWorld.dir/maze_gen_funcs.cpp.o"
+  "CMakeFiles/HelloWorld.dir/maze_gen_funcs.cpp.o.d"
+  "CMakeFiles/HelloWorld.dir/path_planning_algs.cpp.o"
+  "CMakeFiles/HelloWorld.dir/path_planning_algs.cpp.o.d"
+  "HelloWorld"
+  "HelloWorld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
